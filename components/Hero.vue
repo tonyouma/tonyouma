6d214bc6e38e61abfb6e6af35f278eb2ba8b4c20
TonyOuma <kyrietony@gmail.com>
2022-04-09 21:36:04 +0300

    chore: update page contents

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="h-screen items-center flex ">
+  <div class="h-screen items-center flex">
     <div class="">
       <div class="">
         <p class="text-2xl">
@@ -8,21 +8,26 @@
         </p>
         <h1 class="md:text-5xl text-5xl font-bold py-12">
           I am Tony Ouma<br />
-          <span class="text-xl md:text-4xl">UI/UX Designer and Software Developer</span>
+          <span class="text-xl md:text-4xl"
+            >UI/UX Designer and Software Developer</span
+          >
         </h1>
         <p class="text-gray-500 md:w-2/3 mb-24">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos
-          exercitationem atque laudantium perferendis facilis aliquam explicabo
-          minima quasi assumenda officia?
+          Nice to see you here! This website features companies I have worked
+          for, exciting projects I have worked on, and things that I find
+          interesting. With a background in Bsc. Mathematics and Computer
+          Science, I have managed to work for small and agile startups primarily
+          in the field of software design and development.
         </p>
       </div>
       <div class="flex justify-between items-center w-full">
         <div>
-          <button
+          <a
+            href="mailto:oumat61@gmail.com"
             class="py-2 px-8 button rounded-full flex gap-x-4 text items-center font-bold bg-indigo-500 hover:bg-indigo-600 focus:outline-none"
           >
             Reach Out <span><SendIcon /> </span>
-          </button>
+          </a>
         </div>
         <div>
           <a
--- components/Icons/SendIcon.vue
+++ components/Icons/SendIcon.vue
@@ -1,5 +1,5 @@
 <template>
-  <send-icon size="1.2x" class="custom-class"></send-icon>
+  <send-icon size="1.05x" class="custom-class"></send-icon>
 </template>
 
 <script>
