9a02fb7f1c223caf32b7b9a6ba9c67a895de0723
Tony Ouma <oumat621@gmail.com>
2025-08-06 21:44:14 +0300

    refactor: migrate from Nuxt.js to Next.js framework
    - Updated package.json to reflect changes in project name and version, and switched from Nuxt.js to Next.js dependencies and scripts.
    - Removed Vue components and replaced with Next.js structure, including new layout and page files.
    - Added Tailwind CSS configuration and global styles.
    - Introduced TypeScript support with a new tsconfig.json file.
    - Added SVG assets for improved visuals.
    - Deleted unnecessary Vuex store directory and README.

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM node:16.4.2 as build
-WORKDIR /app
-COPY package*.json .
-RUN yarn install
-COPY . .
-RUN yarn run build
-
-FROM nginx:1.13.6 as web
---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div className="font-sans min-h-screen max-w-lg mx-auto pt-32">
+      <main className="">
+        <p className="font-medium pb-2">Tony Ouma</p>
+        <p className="font-medium">
+          Software Engineer <span className="text-gray-300">|</span> Mobile App
+          Developer
+        </p>
+      </main>
+    </div>
+  );
+}
